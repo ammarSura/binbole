617981f9923b0881da88cbe754edb3e06f195f5d
ChatDaddy CI <72265087+ammarSura@users.noreply.github.com>
2025-01-19 11:25:48 +0530

    Initial Commit

---
+++ binbole/binboleApp.swift
@@ -0,0 +1,17 @@
+//
+//  binboleApp.swift
+//  binbole
+//
+//  Created by Ammar Sura on 19/01/25.
+//
+
+import SwiftUI
+
+@main
+struct binboleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
